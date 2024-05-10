fc1fb7fb8cf510c40df177b3d3bdb8fc588dd577
Jonah Venglarcik <jonahvenglarcik@gmail.com>
2024-05-10 14:42:08 -0700

    Create header component

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
 import Image from "next/image";
 import nextConfig from "../next.config.mjs";
+import Header from "@/components/Header";
 const basePath = nextConfig.basePath;
 
 export default function Home() {
   return (
     <>
-      <h1>The Game Graveyard</h1>
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Header />
+      <main className="flex flex-col items-center justify-between p-24">
         <div></div>
       </main>
     </>
---
+++ components/Header.tsx
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <div className="grid p-8 place-content-center text-center">
+      <h1 className="text-4xl font-bold">The Game Graveyard</h1>
+      <p className="pt-4">A list of dead, at-risk, and resurrected online games</p>
+    </div>
+  )
+}
